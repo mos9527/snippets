370392849342f50e7443e8bcf4e0b2b2e39f1ade
greats3an <greats3an@gmail.com>
2025-08-06 16:03:46 +0800

    upd

--- frida/unity/SetTargetFramerate.ts
+++ frida/unity/SetTargetFramerate.ts
@@ -4,5 +4,10 @@ import "frida-il2cpp-bridge";
 Il2Cpp.perform(() => {
     var asm = Il2Cpp.domain.assembly("UnityEngine.CoreModule").image;    
     var application = asm.class("UnityEngine.Application");    
-    application.method("set_targetFrameRate", 1).invoke(120);
+    var set_targetFrameRate = application.method("set_targetFrameRate");
+    set_targetFrameRate.invoke(160);
+    set_targetFrameRate.implementation = function (frameRate) {
+        console.log("* Game setting target frame rate to " + frameRate);
+        return this.method<void>("set_targetFrameRate").invoke(120);
+    }
 });
